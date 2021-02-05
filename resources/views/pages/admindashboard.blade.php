67f5e216591fa0a3b8cc69255d83430e0178d4ca
nadyamarcelinda <77015752+nadyamarcelinda@users.noreply.github.com>
2021-02-05 19:54:37 +0700

    Update admindashboard.blade.php

--- resources/views/pages/admindashboard.blade.php
+++ resources/views/pages/admindashboard.blade.php
@@ -19,7 +19,48 @@
                         @csrf
                         <button class="btn btn-success btn-lg btn-block mt-2" type="submit">APPROVE</button>
                     </form>
+
+                        {{-- <a href="/admin/rejectPage/{{$seminar->id}}" class="btn btn-danger btn-lg btn-block mt-2" type="submit">REJECT</a> --}}
+                        <button type="button" class="btn btn-danger btn-lg btn-block mt-2" data-toggle="modal" data-target="#myModal">REJECT</button>
+
+                        <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+            <h4 class="modal-title">Rejection Message</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          
+        </div>
+        <div class="modal-body">
+          <p>Tell them why their event is rejected.</p>
+          <form class="form-group" method="POST" action="{{ route('reject', $seminar->id) }}">
+            @csrf
+            {{-- <input type="textarea" name="RejectMessage" id=""> --}}
+            <div class="form-group row">
+                <div class="col-md-12">
+                    <input id="RejectMessage" type="textarea" class="form-control @error('RejectMessage') is-invalid @enderror" name="RejectMessage" value="{{ old('RejectMessage') }}" required autocomplete="RejectMessage" autofocus>
                     
+                    @error('RejectMessage')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+            <button class="btn btn-danger btn-lg btn-block mt-2" type="submit">REJECT</button>
+        </form>
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
                 </div>
             </div>
         </div>
