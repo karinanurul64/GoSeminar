7e78508ddea0d7e0e1b7e1060f7f1c70d373e491
nadyamarcelinda <77015752+nadyamarcelinda@users.noreply.github.com>
2021-02-05 19:49:29 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        Blade::directive('currency', function ($expression) {
+            return "Rp <?php echo number_format($expression, 0, ',', '.'); ?>";
+        });
     }
 }
